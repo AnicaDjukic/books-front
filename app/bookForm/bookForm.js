33e63a0a48c56d91065fda9b88b2e663a3685d24
Anica Đukić <anica.djukic99@gmail.com>
2025-03-17 09:48:08 +0100

    project upload

---
+++ app/bookForm/bookForm.js
@@ -0,0 +1,104 @@
+function get() {
+  const urlParams = new URLSearchParams(window.location.search)
+  const id = urlParams.get('id') // Preuzimamo vrednost id parametra upita
+
+  if (!id) { // Ako ne postoji parametar upita, ne dobavljamo knjigu
+    return
+  }
+
+  fetch('http://localhost:63576/api/books/' + id)
+    .then(response => {
+      if (!response.ok) {
+        // Ako statusni kod nije iz 2xx (npr. 404), kreiramo grešku
+        const error = new Error('Request failed. Status: ' + response.status);
+        error.response = response; // Dodajemo ceo response objekat u grešku
+        throw error;  // Bacamo grešku
+      }
+      return response.json()
+    })
+    .then(book => { // Ako je zahtev uspešan, popunjavamo polja sa podacima
+      document.querySelector('#name').value = book.name
+      document.querySelector('#author').value = book.author
+    })
+    .catch(error => {
+      console.error('Error:', error.message)
+      if (error.response && error.response.status === 404) {
+        alert('Book does not exist!')
+      } else {
+        alert('An error occurred while loading the data. Please try again.')
+      }
+    })
+}
+
+get()
+
+function cancel() {
+  window.location.href = '../index.html'
+}
+
+function submit() {
+  const form = document.querySelector('#form')
+  const formData = new FormData(form)
+
+  const reqBody = {
+    name: formData.get('name'),
+    author: formData.get('author')
+  }
+
+  const nameErrorMessage = document.querySelector('#nameError')
+  nameErrorMessage.textContent = ''
+  const authorErrorMessage = document.querySelector('#authorError')
+  authorErrorMessage.textContent = ''
+
+  if (reqBody.name.trim() === '') { // Validacije da uneti podaci nisu prazni
+    nameErrorMessage.textContent = 'Name field is required.'
+    return
+  }
+  if (reqBody.author.trim() === '') {
+    authorErrorMessage.textContent = 'Author field is required.'
+    return
+  }
+
+  let method = 'POST'
+  let url = 'http://localhost:63576/api/books'
+  // Odluka da li je POST ili PUT spram id-ja
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get('id');
+  if (id) {
+    method = 'PUT'
+    url = 'http://localhost:63576/api/books/' + id
+  }
+
+  fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reqBody)
+  })
+    .then(response => {
+      if (!response.ok) {
+        // Ako statusni kod nije iz 2xx (npr. 404), kreiramo grešku
+        const error = new Error('Request failed. Status: ' + response.status);
+        error.response = response; // Dodajemo ceo response objekat u grešku
+        throw error;  // Bacamo grešku
+      }
+      return response.json()
+    })
+    .then(data => {
+      window.location.href = '../index.html'
+    })
+    .catch(error => {
+      console.error('Error:', error.message)
+      // Bitna provera ako je u pitanju ažuriranje knjige
+      if (error.response && error.response.status === 404) {
+        alert('Book does not exist!')
+      }
+      else if (error.response && error.response.status === 400) {
+        alert('Data is invalid!')
+      }
+      else {
+        alert('An error occurred while updating the data. Please try again.')
+      }
+    })
+}
---
+++ app/index.js
@@ -0,0 +1,108 @@
+function getAll() {
+  fetch('http://localhost:63576/api/books') // Pravi GET zahtev da dobavi sve knjige sa servera
+    .then(response => {
+      if (!response.ok) {
+        // Ako se vrati statusni kod koji nije iz 2xx, tretiraj kao grešku
+        throw new Error('Request failed. Status: ' + response.status)
+      }
+      return response.json()
+    })
+    .then(books => renderData(books))       // Ako su podaci ispravni, prikaži ih u HTMLu
+    .catch(error => {                       // Ako podaci nisu ispravni, sakrij tabelu i prikaži poruku o grešci
+      console.error('Error:', error.message)
+      // Sakrij tabelu
+      let table = document.querySelector('table')
+      if (table) {
+        table.style.display = 'none'
+      }
+      // Prikaži poruku o grešci
+      alert('An error occurred while loading the data. Please try again.')
+    })
+}
+
+function renderData(data) {
+  let table = document.querySelector('table tbody')
+  // Ispraznimo tabelu pre nego što dodamo nove podatke
+  table.innerHTML = '';  // Ovo briše sve redove u tabeli
+
+  let tableHeader = document.querySelector('table thead') // Zaglavlje tabele
+
+  // Ako lista podataka (data) je prazna
+  if (data.length === 0) {
+    // Sakrij zaglavlje tabele
+    tableHeader.classList.add('hidden')
+
+    // Prikaži poruku da nema podataka za prikazivanje
+    let noDataMessage = document.querySelector('#no-data-message')
+    noDataMessage.classList.remove('hidden')
+
+  } else {
+    // Ukloniti poruku da nema podataka za prikazivanje
+    let noDataMessage = document.querySelector('#no-data-message')
+    noDataMessage.classList.add('hidden')
+
+    // Prikazivanje zaglavlja tabele
+    tableHeader.classList.remove('hidden')
+
+    // Za svaku knjigu dodajemo red u tabeli koji ima četiri ćelije (naziv, autor, edit, delete)
+    data.forEach(book => {
+      let newRow = document.createElement('tr')
+
+      // Kreiranje ćelije za naziv knjige
+      let cell1 = document.createElement('td')
+      cell1.textContent = book['name']
+      newRow.appendChild(cell1)
+
+      // Kreiranje ćelije za autora
+      let cell2 = document.createElement('td')
+      cell2.textContent = book['author']
+      newRow.appendChild(cell2)
+
+      // Kreiranje ćelije za dugme "Edit"
+      let cell3 = document.createElement('td')
+      let editButton = document.createElement('button')
+      editButton.textContent = 'Edit'
+      editButton.addEventListener('click', function () {
+        window.location.href = './bookForm/bookForm.html?id=' + book['id']
+      })
+      cell3.appendChild(editButton)
+      newRow.appendChild(cell3)
+
+      // Kreiranje ćelije za dugme "Delete"
+      let cell4 = document.createElement('td')
+      let deleteButton = document.createElement('button')
+      deleteButton.textContent = 'Delete'
+      deleteButton.addEventListener('click', function () {
+        fetch('http://localhost:63576/api/books/' + book['id'], { method: 'DELETE' })
+          .then(response => {
+            if (!response.ok) {
+              // Ako statusni kod nije iz 2xx (npr. 404), kreiramo grešku
+              const error = new Error('Request failed. Status: ' + response.status);
+              error.response = response; // Dodajemo ceo response objekat u grešku
+              throw error;  // Bacamo grešku
+            }
+            getAll() // Ponovo učitaj podatke nakon brisanja
+          })
+          .catch(error => {
+            console.error('Error:', error.message)
+            if (error.response && error.response.status === 404) {
+              alert('Book does not exist!')
+            } else {
+              alert('An error occurred while deleting the book. Please try again.')
+            }
+          })
+      })
+      cell4.appendChild(deleteButton)
+      newRow.appendChild(cell4)
+
+      // Dodavanje novog reda u tabelu
+      table.appendChild(newRow)
+    })
+  }
+}
+
+function add() {
+  window.location.href='./bookForm/bookForm.html'
+}
+
+document.addEventListener('DOMContentLoaded', getAll)
